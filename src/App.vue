88689c2d60d783468a4697a0bd811305c7e3d80c
范勤阳 <fqymtu@gmail.com>
2025-03-20 17:22:46 +0800

    增加 vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { Analytics } from '@vercel/analytics/vue'
 </script>
 
 <template>
+  <Analytics />
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
